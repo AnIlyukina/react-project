ac952d1519516b62ce50729cb2737d827bd71c37
Ilyukina <anyailyukina@yandex.ru>
2023-09-23 22:48:05 +0300

    add config

---
+++ config/build/types/config.ts
@@ -0,0 +1,21 @@
+import {strict} from "assert";
+
+export type BuildMode = 'production' | 'development';
+
+export interface BuildPaths {
+    entry: string;
+    build: string;
+    html: string;
+}
+
+export interface BuildEnv {
+    mode: BuildMode;
+    port: number;
+}
+
+export interface BuildOptions {
+    mode: BuildMode;
+    paths:BuildPaths;
+    isDev: boolean;
+    port: number;
+}
