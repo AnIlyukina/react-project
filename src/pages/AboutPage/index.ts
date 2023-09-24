a5de9036e50dee757dd3a02edc719a87b52fe8ee
Ilyukina <anyailyukina@yandex.ru>
2023-09-24 23:19:37 +0300

    настройка абсолютных путей, архитектура проекта(feature-sliced)

---
+++ src/pages/AboutPage/index.ts
@@ -0,0 +1,5 @@
+import { AboutPagesAsync} from "./ui/AboutPages.async";
+
+export {
+    AboutPagesAsync as AboutPage
+}
---
+++ src/pages/MainPage/index.ts
@@ -0,0 +1,5 @@
+import { MainPagesAsync} from "./ui/MainPages.async";
+
+export {
+    MainPagesAsync as MainPage
+}
