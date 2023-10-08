7620909c357bba098a5f8f30c636173ef544252d
Ilyukina <anyailyukina@yandex.ru>
2023-10-08 16:44:29 +0300

    jest

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,7 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import {BuildOptions} from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-
+import {BundleAnalyzerPlugin}  from 'webpack-bundle-analyzer';
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
         new webpack.ProgressPlugin(),
@@ -16,6 +16,10 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+
+        })
     ];
 }
