f10fb154e8971dcb5097245cf75c47be02b7ef73
Ilyukina <anyailyukina@yandex.ru>
2023-10-14 23:51:50 +0300

    фикс dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,7 +4,8 @@ import {BuildOptions} from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BundleAnalyzerPlugin}  from 'webpack-bundle-analyzer';
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+
+    const plugins = [
         new webpack.ProgressPlugin(),
         new HtmlWebpackPlugin({
             template: paths.html,
@@ -16,10 +17,14 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
+    ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push( new BundleAnalyzerPlugin({
             openAnalyzer: false,
 
-        })
-    ];
+        }));
+    }
+    return plugins;
 }
