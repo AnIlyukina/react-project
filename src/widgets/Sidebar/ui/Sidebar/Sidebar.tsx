383011d56f1e3226ca9d89f98211b259f78a1b46
Ilyukina <anyailyukina@yandex.ru>
2023-10-15 00:03:43 +0300

    фикс linter ts

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,11 +3,14 @@ import styles from './Sidebar.module.scss';
 import React, { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import {useTranslation} from 'react-i18next';
 
 interface SidebarProps {
   className?: string
 }
 export const Sidebar = ({ className }: SidebarProps) => {
+
+    const { t } = useTranslation();
     const [collapsed, setCollapsed] = useState(false);
 
     const onToggle = () => {
@@ -18,12 +21,12 @@ export const Sidebar = ({ className }: SidebarProps) => {
             data-testid="sidebar"
             className={classNames(styles.Sidebar, { [styles.collapsed]: collapsed }, [className])}
         >
-            {/*<button*/}
-            {/*    data-testid="sidebar-toggle"*/}
-            {/*    onClick={onToggle}*/}
-            {/*>*/}
-            {/*    Toggle*/}
-            {/*</button>*/}
+            <button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+            >
+                {t('Перевести')}
+            </button>
 
             <div className={styles.switchers}>
                 <ThemeSwitcher/>
