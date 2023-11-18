7a4f8b620e572bfe3eb26918d35a6021d5ed1a19
Ilyukina <anyailyukina@yandex.ru>
2023-11-18 19:48:31 +0300

    add new theme, add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import {BuildOptions} from './types/config';
 import {buildCssLoaders} from './loaders/buildCssLoaders';
+import {buildBabelLoader} from './loaders/buildBabelLoader';
 
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
@@ -27,22 +28,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
-    const babelLoader = {
-        test: /\.(?:js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                'plugins': [
-                    ['i18next-extract', {
-                        locales:['ru', 'en'],
-                        keyIsDefaultValue: true,
-                    }],
-                ]
-            }
-        }
-    };
+    const babelLoader = buildBabelLoader(isDev);
 
     return [
         cssLoader,
