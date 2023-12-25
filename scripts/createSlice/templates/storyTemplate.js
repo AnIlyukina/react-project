fbaa72cf2fd0fdf03f4db8491c8d9a8efbb59d31
annaIlyukina <aailyukina@scid.ru>
2023-12-25 14:17:25 +0300

    add generate slice script

---
+++ scripts/createSlice/templates/storyTemplate.js
@@ -0,0 +1,21 @@
+module.exports = (layer, componentName) => `import React from 'react';
+import type {Meta, StoryObj} from '@storybook/react';
+
+import { ${componentName} } from './${componentName}';
+
+const meta = {
+    title: '${layer}/${componentName}',
+    component: ${componentName},
+    tags: ['autodocs'],
+    argTypes: {},
+} satisfies Meta<typeof ${componentName}>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+
+export const Normal = Story = {
+    args: {},
+};
+`;
